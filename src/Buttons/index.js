c2604ad3a84078da44c19895db0a006be46da593
Beata Ambroziak <beataxambroziak@gmail.com>
2023-06-17 12:38:07 +0200

    renamed class and added new classes and properties

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,13 +1,13 @@
 import "./style.css";
 
 const Buttons = ({ tasks, hideDoneTasks }) => (
-tasks.length > 0 && (
-        <span>
-            <button className="buttons">
+    tasks.length > 0 && (
+        <span className="buttons">
+            <button className="buttons__button">
                 {hideDoneTasks && tasks.some(({ done }) => done) ? "Pokaż" : "Ukryj"} ukończone
             </button>
             <button
-                className="buttons"
+                className="buttons__button"
                 disabled={tasks.every(({ done }) => done)}
             >
                 Ukończ wszystkie
